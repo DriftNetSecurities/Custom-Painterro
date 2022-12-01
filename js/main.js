266c1a03994e0e32319aaeb69ca42df7295fe999
Emanuel Sanchez <88672572+dns-emanuels@users.noreply.github.com>
2022-12-01 09:40:17 -0600

    grid function test

--- js/main.js
+++ js/main.js
@@ -601,7 +601,7 @@ class PainterroProc {
       '<div class="ptro-scroller">' +
       '<div class="ptro-center-table">' +
       '<div class="ptro-center-tablecell">' +
-      '<div">THIS IS A TEST INNER GRID MAYBE GOES HERE</div>' +
+      `<div>${this.drawGrid()}</div>` +
       `<canvas id="${this.id}-canvas"></canvas>` +
       `<div class="ptro-substrate"></div>${cropper}` +
       "</div>" +
@@ -849,6 +849,40 @@ class PainterroProc {
 
   getBtnEl(tool) {
     return this.getElemByIdSafe(tool.buttonId);
+  }
+
+  drawGrid() {
+    let ctx = this.$el.getContext("2d");
+
+    let s = 28;
+    let nX = Math.floor(this.width / s) - 2;
+    let nY = Math.floor(this.height / s) - 2;
+    let pX = this.width - nX * s;
+    let pY = this.height - nY * s;
+
+    // Bonus code for choosing nX instead of s
+    /* let nX = 20
+				let s = Math.floor(this.width / (nX + 2))
+				let pX = this.width - nX * s
+				let nY = Math.floor((this.height - pX) / (this.width - pX) * nX)
+				let pY = this.height - nY * s */
+
+    let pL = Math.ceil(pX / 2) - 0.5;
+    let pT = Math.ceil(pY / 2) - 0.5;
+    let pR = this.width - nX * s - pL;
+    let pB = this.height - nY * s - pT;
+
+    ctx.strokeStyle = "lightgrey";
+    ctx.beginPath();
+    for (var x = pL; x <= this.width - pR; x += s) {
+      ctx.moveTo(x, pT);
+      ctx.lineTo(x, this.height - pB);
+    }
+    for (var y = pT; y <= this.height - pB; y += s) {
+      ctx.moveTo(pL, y);
+      ctx.lineTo(this.width - pR, y);
+    }
+    ctx.stroke();
   }
 
   save() {
