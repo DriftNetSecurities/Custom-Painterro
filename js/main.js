fd994e91d6f2b3a8317c13b01f20ccbf2fe7146c
Emanuel Sanchez <88672572+dns-emanuels@users.noreply.github.com>
2022-12-01 10:13:05 -0600

    testing functions

--- js/main.js
+++ js/main.js
@@ -602,7 +602,7 @@ class PainterroProc {
       '<div class="ptro-scroller">' +
       '<div class="ptro-center-table">' +
       '<div class="ptro-center-tablecell">' +
-      '<div">THIS IS A TEST INNER GRID MAYBE GOES HERE</div>' +
+      `<div>THIS IS A TEST INNER GRID MAYBE GOES HERE</div> ${this.testFunction()}` +
       `<canvas id="${this.id}-canvas"></canvas>` +
       `<div class="ptro-substrate"></div>${cropper}` +
       "</div>" +
@@ -852,62 +852,11 @@ class PainterroProc {
     return this.getElemByIdSafe(tool.buttonId);
   }
 
-  gridFunction() {
-    let canvas, ctx;
-
-    // draws a grid
-    function createGrid() {
-      // draw a line every *step* pixels
-      const step = 50;
-
-      // our end points
-      const width = canvas.width;
-      const height = canvas.height;
-
-      // set our styles
-      ctx.save();
-      ctx.strokeStyle = "gray"; // line colors
-      ctx.fillStyle = "black"; // text color
-      ctx.font = "14px Monospace";
-      ctx.lineWidth = 0.35;
-
-      // draw vertical from X to Height
-      for (let x = 0; x < width; x += step) {
-        // draw vertical line
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, height);
-        ctx.stroke();
-
-        // draw text
-        ctx.fillText(x, x, 12);
-      }
-
-      // draw horizontal from Y to Width
-      for (let y = 0; y < height; y += step) {
-        // draw horizontal line
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(width, y);
-        ctx.stroke();
-
-        // draw text
-        ctx.fillText(y, 0, y);
-      }
-
-      // restore the styles from before this function was called
-      ctx.restore();
-    }
-
-    function loadGrid() {
-      // set our config variables
-      canvas = document.getElementById(`${this.id}`);
-      ctx = canvas.getContext("2d");
-
-      createGrid();
-    }
-
-    document.addEventListener("DOMContentLoaded", loadGrid);
+  testFunction() {
+    console.log("TEST");
+    return (
+      <div>hello world idk this is a test to see if i can return html</div>
+    );
   }
 
   save() {
